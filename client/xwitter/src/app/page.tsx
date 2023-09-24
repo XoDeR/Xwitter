ecb0cefa3364a73a54bf97ef9c9fae62cff1bec6
Volodymyr <XoDeR@users.noreply.github.com>
2023-09-24 15:23:35 +0200

    client: sidebar design

--- client/xwitter/src/app/page.tsx
+++ client/xwitter/src/app/page.tsx
@@ -1,7 +1,12 @@
 import { BiHomeCircle, BiUser } from "react-icons/bi";
-import { BsBell, BsBookmark, BsTwitter } from "react-icons/bs";
+import {
+  BsBell,
+  BsBookmark,
+  BsTwitter,
+  BsEnvelope,
+  BsThreeDots,
+} from "react-icons/bs";
 import { HiOutlineHashtag } from "react-icons/hi";
-import { HiEnvelope } from "react-icons/hi2";
 import Link from "next/link";
 
 const NAVIGATION_ITEMS = [
@@ -23,7 +28,7 @@ const NAVIGATION_ITEMS = [
   },
   {
     title: "Messages",
-    icon: HiEnvelope,
+    icon: BsEnvelope,
   },
   {
     title: "Bookmarks",
@@ -40,19 +45,36 @@ const Home = () => {
     <div className="w-full h-full flex justify-center items-center relative bg-black">
       <div className="max-w-screen-xl w-full h-full flex relative">
         {/* left sidebar, nav/header */}
-        <section className="fixed w-72 flex flex-col h-screen space-y-4 my-4">
-          {NAVIGATION_ITEMS.map((item) => (
-            <Link
-              className="hover:bg-white/10 text-3xl transition duration-200 flex items-center justify-start w-fit space-x-4 rounded-3xl py-2 px-6"
-              href={`/${item.title.toLowerCase()}`}
-              key={item.title}
-            >
-              <div>
-                <item.icon />
+        <section className="fixed w-[275px] flex flex-col items-stretch h-screen">
+          <div className="flex flex-col items-stretch h-full space-y-4 my-4">
+            {NAVIGATION_ITEMS.map((item) => (
+              <Link
+                className="hover:bg-white/10 text-3xl transition duration-200 flex items-center justify-start w-fit space-x-4 rounded-3xl py-2 px-6"
+                href={`/${item.title.toLowerCase()}`}
+                key={item.title}
+              >
+                <div>
+                  <item.icon />
+                </div>
+                {item.title !== "Twitter" && <div>{item.title}</div>}
+              </Link>
+            ))}
+            <button className="rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200">
+              Tweet
+            </button>
+          </div>
+          <button className="rounded-full flex items-center space-x-2 m-4 bg-transparent p-4 text-center hover:bg-white/20 transition duration-200 w-full">
+            <div className="flex items-center space-x-2">
+              <div className="rounded-full bg-slate-400 w-8 h-8"></div>
+              <div className="text-left text-sm">
+                <div className="font-semibold">RIO</div>
+                <div className="">@realintorg</div>
               </div>
-              {item.title !== "Twitter" && <div>{item.title}</div>}
-            </Link>
-          ))}
+              <div>
+                <BsThreeDots />
+              </div>
+            </div>
+          </button>
         </section>
         {/* <main>home timeline</main>
         <section>right section</section> */}
