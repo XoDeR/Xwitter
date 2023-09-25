1501d8c85da182200308533dda22159866fefa6d
Volodymyr <XoDeR@users.noreply.github.com>
2023-09-25 13:33:06 +0200

    Client: moved LeftSidebar to a separate component

---
+++ client/xwitter/src/app/components/LeftSidebar.tsx
@@ -0,0 +1,79 @@
+import { BiHomeCircle, BiUser } from "react-icons/bi";
+import {
+  BsBell,
+  BsBookmark,
+  BsTwitter,
+  BsEnvelope,
+  BsThreeDots,
+} from "react-icons/bs";
+import { HiOutlineHashtag } from "react-icons/hi";
+import Link from "next/link";
+
+const NAVIGATION_ITEMS = [
+  {
+    title: "Twitter",
+    icon: BsTwitter,
+  },
+  {
+    title: "Home",
+    icon: BiHomeCircle,
+  },
+  {
+    title: "Explore",
+    icon: HiOutlineHashtag,
+  },
+  {
+    title: "Notifications",
+    icon: BsBell,
+  },
+  {
+    title: "Messages",
+    icon: BsEnvelope,
+  },
+  {
+    title: "Bookmarks",
+    icon: BsBookmark,
+  },
+  {
+    title: "Profile",
+    icon: BiUser,
+  },
+];
+
+const LeftSidebar = () => {
+  return (
+    <section className="fixed w-[275px] flex flex-col items-stretch h-screen">
+      <div className="flex flex-col items-stretch h-full space-y-4 my-4">
+        {NAVIGATION_ITEMS.map((item) => (
+          <Link
+            className="hover:bg-white/10 text-3xl transition duration-200 flex items-center justify-start w-fit space-x-4 rounded-3xl py-2 px-6"
+            href={`/${item.title.toLowerCase()}`}
+            key={item.title}
+          >
+            <div>
+              <item.icon />
+            </div>
+            {item.title !== "Twitter" && <div>{item.title}</div>}
+          </Link>
+        ))}
+        <button className="rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200">
+          Tweet
+        </button>
+      </div>
+      <button className="rounded-full flex items-center space-x-2 m-4 bg-transparent p-4 text-center hover:bg-white/20 transition duration-200 w-full">
+        <div className="flex items-center space-x-2">
+          <div className="rounded-full bg-slate-400 w-12 h-12"></div>
+          <div className="text-left text-sm">
+            <div className="font-semibold">RIO</div>
+            <div className="">@realintorg</div>
+          </div>
+          <div>
+            <BsThreeDots />
+          </div>
+        </div>
+      </button>
+    </section>
+  );
+};
+
+export default LeftSidebar;
