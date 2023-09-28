751ec93ed03997fcf7e6e8cac02da4c4c695983d
Volodymyr <XoDeR@users.noreply.github.com>
2023-09-28 17:51:12 +0200

    Client: added elements to right panel

--- client/xwitter/src/app/page.tsx
+++ client/xwitter/src/app/page.tsx
@@ -12,8 +12,8 @@ const Home = () => {
         {/* main */}
         <MainComponent />
         {/* right section */}
-        <section className="w-full sticky top-2 mt-2 flex flex-col items-stretch h-screen px-6">
-          <div>
+        <section className="w-full sticky top-2 no-scrollbar overflow-y-auto mt-2 flex flex-col items-stretch h-screen px-6">
+          <div className="sticky top-2">
             <div className="relative w-full h-full group">
               <input
                 id="searchBox"
@@ -29,7 +29,44 @@ const Home = () => {
               </label>
             </div>
           </div>
-          <div></div>
+          <div className="flex flex-col rounded-xl bg-neutral-900 my-4">
+            <h3 className="font-bold text-xl my-4 px-4">
+              What&apos;s happening
+            </h3>
+            <div>
+              {Array.from({ length: 5 }).map((_, i) => (
+                <div
+                  key={i}
+                  className="hover:bg-white/10 p-4 last:rounded-b-xl transition duration-200"
+                >
+                  <div className="font-bold text-lg">#trending{i + 1}</div>
+                  <div className="text-xs text-neutral-400">56.7k</div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col rounded-xl bg-neutral-900 my-4 sticky top-0">
+            <h3 className="font-bold text-xl my-4 px-4">Who to follow</h3>
+            <div>
+              {Array.from({ length: 5 }).map((_, i) => (
+                <div
+                  key={i}
+                  className="hover:bg-white/10 p-4 flex items-center last:rounded-b-xl transition duration-200"
+                >
+                  <div className="w-10 h-10 bg-neutral-600 rounded-full"></div>
+                  <div className="flex flex-col space-y-2">
+                    <div>Other user</div>
+                    <div>@otheruser9876</div>
+                  </div>
+                  <div>
+                    <button className="rounded-full px-6 py-2 bg-white text-neutral-950">
+                      Follow
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
           <div></div>
         </section>
       </div>
