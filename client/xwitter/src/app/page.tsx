e263e59f6967fbf41aa862857bf1a7ef41a6d71a
Volodymyr <XoDeR@users.noreply.github.com>
2023-09-29 14:35:24 +0200

    moved components to appropriate folder

--- client/xwitter/src/app/page.tsx
+++ client/xwitter/src/app/page.tsx
@@ -1,10 +1,10 @@
-import LeftSidebar from "./components/LeftSidebar";
-import MainComponent from "./components/MainComponent";
-import RightSection from "./components/RightSection";
+import LeftSidebar from "@/components/LeftSidebar";
+import MainComponent from "@/components/MainComponent";
+import RightSection from "@/components/RightSection";
 
 const Home = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center relative bg-black">
+    <div className="w-full h-full flex justify-center items-center text-white relative bg-black">
       <div className="max-w-[70vw] w-full h-full flex relative">
         {/* left sidebar, nav/header */}
         <LeftSidebar />
--- client/xwitter/tailwind.config.js
+++ client/xwitter/tailwind.config.js
@@ -54,7 +54,7 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        default: ["var(--font-sans)", ...fontFamily.sans],
       },
       keyframes: {
         "accordion-down": {
