0b1ed35d0e5478a74bdff5e2fd5ad7eddc7f4a13
Volodymyr <XoDeR@users.noreply.github.com>
2023-10-16 22:58:38 +0200

    db schema corrections

--- client/xwitter/src/lib/db/schema.ts
+++ client/xwitter/src/lib/db/schema.ts
@@ -2,33 +2,48 @@ import { text, timestamp, uuid, pgTable } from "drizzle-orm/pg-core";
 
 export const profiles = pgTable("profiles", {
   id: uuid("id").primaryKey().defaultRandom(),
-  updated_at: timestamp("updated_at").defaultNow().notNull(),
-  username: text("username"),
-  full_name: text("full_name"),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  username: text("username").notNull(),
+  fullName: text("full_name").notNull(),
 });
 
 export const tweets = pgTable("tweets", {
   id: uuid("id").primaryKey().defaultRandom(),
-  text: text("text"),
-  user_id: uuid("user_id"),
-  created_at: timestamp("created_at"),
-  updated_at: timestamp("updated_at"),
+  text: text("text").notNull(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => profiles.id),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
 
 export const hashtags = pgTable("hashtags", {
   id: uuid("id").primaryKey().defaultRandom(),
-  name: text("name"),
+  name: text("name").notNull(),
 });
 
-export const tweet_hashtag = pgTable("tweet_hashtag", {
-  tweet_id: uuid("tweet_id").primaryKey().defaultRandom(),
-  hashtag_id: uuid("hashtag_id").primaryKey(),
-});
+export const tweetHashtag = pgTable(
+  "tweet_hashtag",
+  {
+    tweetId: uuid("tweet_id")
+      .notNull()
+      .references(() => tweets.id),
+    hashtagId: uuid("hashtag_id")
+      .notNull()
+      .references(() => hashtags.id),
+  },
+  (tweet_hashtag) => ({
+    tweetHashtagPrimaryKey: primaryKey(
+      tweet_hashtag.tweetId,
+      tweet_hashtag.hashtagId
+    ),
+  })
+);
 
 export const replies = pgTable("replies", {
   id: uuid("id").primaryKey().defaultRandom(),
-  text: text("text"),
-  user_id: uuid("user_id"),
+  text: text("text").notNull(),
+  user_id: uuid("user_id").notNull(),
   tweet_id: uuid("tweet_id"),
   reply_id: uuid("reply_id"),
 });
